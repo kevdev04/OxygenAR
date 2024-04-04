908a4fb2a416a1bc823ce79c54cb6aef64f2bdf4
JAntonioAlvarado <antonio02jaas@gmail.com>
2024-04-04 00:26:13 -0600

    Mapa nuevo

--- lib/views/mapview.dart
+++ lib/views/mapview.dart
@@ -1,5 +1,7 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geocoding/geocoding.dart';
 
 void main() => runApp(const MapView());
 
@@ -23,22 +25,91 @@ class MapScreen extends StatefulWidget {
 class _MapScreenState extends State<MapScreen> {
   late GoogleMapController mapController;
 
-  final LatLng _initialPosition = LatLng(19.432608, -99.133209);
+  final LatLng _initialPosition = const LatLng(19.432608, -99.133209);
+  final Set<Marker> _markers = {};
+
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+    _setMapStyle();
+  }
+
+void _addMarker(LatLng position) {
+    setState(() {
+      _markers.clear();
+      _markers.add(Marker(
+        markerId: MarkerId(position.toString()),
+        position: position,
+      ));
+    });
+  }
+
+ Future<void> _setMapStyle() async {
+    String style = await DefaultAssetBundle.of(context).loadString("assets/map_style.json");
+    mapController.setMapStyle(style);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: CameraPosition(
-          target: _initialPosition,
-          zoom: 11.0,
-        ),
+      appBar: AppBar(
+        title: const Text('Mapa con Búsqueda de Direcciones'),
+      ),
+       body: Stack(
+        children: <Widget>[
+          GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: CameraPosition(
+              target: _initialPosition,
+              zoom: 11.0,
+            ),
+            mapType: MapType.normal,
+            markers: _markers,
+            onTap: _addMarker,
+          ),
+          Positioned(
+            top: 20.0,
+            left: 20.0,
+            right: 20.0,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black12,
+                    offset: Offset(0, 2),
+                    blurRadius: 6.0,
+                  ),
+                ],
+              ),
+              child: TextField(
+                decoration: const InputDecoration(
+                  hintText: 'Buscar dirección',
+                  border: InputBorder.none,
+                  suffixIcon: Icon(Icons.search),
+                ),
+                onSubmitted: (value) async {
+                  List<Location> locations = await locationFromAddress(value);
+                  if (locations.isNotEmpty) {
+                    Location location = locations.first;
+                    LatLng latLng = LatLng(location.latitude, location.longitude);
+                    _addMarker(latLng);
+                    // Mueve la cámara del mapa a la nueva ubicación
+                    mapController.animateCamera(CameraUpdate.newLatLngZoom(latLng, 15.0));
+                  } else {
+                    // No se encontró ninguna ubicación para la dirección proporcionada
+                    // Puedes mostrar un mensaje de error al usuario o realizar otra acción apropiada.
+                  }
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
+
+
